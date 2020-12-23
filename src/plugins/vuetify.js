0a3c0e46d0621adb6d7559573508832b43516ddc
Majid Ghasemi <mjghcr@gmail.com>
2020-12-24 03:14:25 +0330

    custom color pallette created

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        form_primary: '#eceeef',
+        form_text: '#707070',
+        primary: '#1c7cd5',
+        error_alert: '#efdfdf',
+        error_alert_text: '#9f4f48',
+        charcoal: '#373a3c',
+        logout: '#5bc0de',
+      },
+    },
+  },
+});
