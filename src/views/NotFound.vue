95fc82ecdc034cf53a166db2e018ca2d040bbaf0
Majid Ghasemi <mjghcr@gmail.com>
2020-12-25 01:27:37 +0330

    Not found component created, wrapper for 404 error

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center" class="text-center">
+        <v-col cols="12" class="mt-8 mb-4">
+            <h1 class="display-3 error_alert_text--text"> <span class="red--text"> OooOps!</span> Something went wrong</h1>
+        </v-col>
+        <v-col>
+            <h3 class="display-2"> <router-link :to="{ name: 'Login' }" class="green--text text-decoration-none">Back to safety <v-icon class="green--text display-2">mdi-arrow-collapse-right</v-icon> </router-link> </h3>
+        </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: "not-found"
+}
+</script>
