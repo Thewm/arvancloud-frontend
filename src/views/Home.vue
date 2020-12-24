4f3db208b457d44987129aa974264352154d0cf7
Majid Ghasemi <mjghcr@gmail.com>
2020-12-25 01:25:30 +0330

    Home page for app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container fill-height>
+      <v-row class="text-center">
+        <v-col cols="12">
+          <h1 class="font-weight-thin display-4">Arvan Front-End Challenge</h1>
+        </v-col>
+        <v-col>
+          <v-btn x-large outlined class="px-16" :to="{ name: 'Login' }">Login</v-btn>
+        </v-col>
+      </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
 };
 </script>
