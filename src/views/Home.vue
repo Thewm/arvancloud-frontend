b664dcffc5f6c69244327cedac9a4c58e5004fa2
Majid Ghasemi <mjghcr@gmail.com>
2020-12-26 03:04:27 +0330

    Prettier warnings fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,20 @@
 <template>
   <v-container fill-height>
-      <v-row class="text-center">
-        <v-col cols="12">
-          <h1 class="font-weight-thin display-4">Arvan Front-End Challenge</h1>
-        </v-col>
-        <v-col>
-          <v-btn x-large outlined class="px-16" :to="{ name: 'Login' }">Login</v-btn>
-        </v-col>
-      </v-row>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h1 class="font-weight-thin display-4">Arvan Front-End Challenge</h1>
+      </v-col>
+      <v-col>
+        <v-btn x-large outlined class="px-16" :to="{ name: 'Login' }"
+          >Login</v-btn
+        >
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-
 export default {
-  name: "Home",
+  name: "Home"
 };
 </script>
