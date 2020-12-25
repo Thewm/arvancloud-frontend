eea3464aed7e9514453d908281fb46a8397a7b56
Majid Ghasemi <mjghcr@gmail.com>
2020-12-26 03:06:54 +0330

    some responsive edge cases added

--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -1,40 +1,55 @@
 <template>
-  <v-app-bar
-    color="charcoal"
-    flat
-    app
-    dark
-    clipped-left
-  >
+  <v-app-bar color="charcoal" flat app dark clipped-left>
     <div class="d-md-flex align-center">
-      <span class="headline mr-6" :class="[$vuetify.breakpoint.mdAndUp ? 'headline mr-6' : 'title mr-3']">Arvan Challenge</span>
-      <span :class="[$vuetify.breakpoint.mdAndUp ? 'title font-weight-thin' : 'subtitle-2 font-weight-thin']">Welcome {{ currentUser.username || guest }} </span>
+      <span
+        class="headline mr-6"
+        :class="[$vuetify.breakpoint.mdAndUp ? 'headline mr-6' : 'title mr-3']"
+        >Arvan Challenge</span
+      >
+      <span
+        :class="[
+          $vuetify.breakpoint.mdAndUp
+            ? 'title font-weight-thin text-capitalize'
+            : 'subtitle-2 font-weight-thin text-capitalize'
+        ]"
+        >Welcome {{ currentUser.username || guest }}
+      </span>
     </div>
     <v-spacer></v-spacer>
     <div class="mr-4">
-      <v-btn color="logout" outlined :class="[$vuetify.breakpoint.smAndUp ? 'px-4 text-capitalize' : 'px-2 text-capitalize']" @click="logout">Logout</v-btn>
+      <v-btn
+        color="logout"
+        outlined
+        :class="[
+          $vuetify.breakpoint.smAndUp
+            ? 'px-4 text-capitalize'
+            : 'px-2 text-capitalize'
+        ]"
+        @click="logout"
+        >Logout</v-btn
+      >
     </div>
   </v-app-bar>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import { LOGOUT } from "@/store/actions.type"
+import { LOGOUT } from "@/store/actions.type";
 
 export default {
-    name: "Header",
-    data: () => ({
-      guest: "guest"
-    }),
-    computed: {
-      ...mapGetters(["currentUser"]),
-    },
-    methods: {
-      logout() {
-        this.$store.dispatch(LOGOUT).then(() => {
-          this.$router.push({ name: "Login" });
-        });
-      }
+  name: "Header",
+  data: () => ({
+    guest: "guest"
+  }),
+  computed: {
+    ...mapGetters(["currentUser"])
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch(LOGOUT).then(() => {
+        this.$router.push({ name: "Login" });
+      });
     }
-}
+  }
+};
 </script>
