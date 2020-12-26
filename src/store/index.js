7c61996c92b5b73b1cfc68a455213c5c0bcff4c4
Majid Ghasemi <mjghcr@gmail.com>
2020-12-26 14:33:46 +0330

    deleting unnecessary import for deleted module (user module)

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import auth from "./auth.module";
-import user from "./user.modules";
 import article from "./article.modules";
 
 Vue.use(Vuex);
@@ -10,7 +9,6 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     auth,
-    user,
     article
   }
 });
