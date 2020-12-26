db2f61e0f5b5926974eeeb651856b362028fc2f0
Majid Ghasemi <mjghcr@gmail.com>
2020-12-26 14:29:15 +0330

    Authentication modules, source store for login, logout and register

--- src/store/auth.module.js
+++ src/store/auth.module.js
@@ -1,10 +1,10 @@
 import ApiService from "@/common/service.api";
 import JwtService from "@/common/service.jwt";
 import { LOGIN, LOGOUT, REGISTER, CHECK_AUTH } from "./actions.type";
-import { SET_AUTH, PURGE_AUTH, SET_ERROR } from "./mutations.type";
+import { SET_AUTH, CLEAN_AUTH, SET_ERROR } from "./mutations.type";
 
 const state = {
-  errors: false,
+  errors: {},
   user: {},
   isAuthenticated: !!JwtService.getToken()
 };
@@ -32,7 +32,7 @@ const actions = {
     });
   },
   [LOGOUT](context) {
-    context.commit(PURGE_AUTH);
+    context.commit(CLEAN_AUTH);
   },
   [REGISTER](context, credentials) {
     return new Promise((resolve, reject) => {
@@ -58,14 +58,14 @@ const actions = {
           context.commit(SET_ERROR, response.data.errors);
         });
     } else {
-      context.commit(PURGE_AUTH);
+      context.commit(CLEAN_AUTH);
     }
   }
 };
 
 const mutations = {
-  [SET_ERROR](state) {
-    state.errors = true;
+  [SET_ERROR](state, error) {
+    state.errors = error;
   },
   [SET_AUTH](state, user) {
     state.isAuthenticated = true;
@@ -73,7 +73,7 @@ const mutations = {
     state.errors = {};
     JwtService.saveToken(state.user.token);
   },
-  [PURGE_AUTH](state) {
+  [CLEAN_AUTH](state) {
     state.isAuthenticated = false;
     state.user = {};
     state.errors = {};
